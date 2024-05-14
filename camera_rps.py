2242da7dec4d2ade8557e41fc4c3254e51953470
shay-94 <akshayptl26@gmail.com>
2024-05-14 16:58:44 +0100

    Added more clear and concise Docstrings

--- camera_rps.py
+++ camera_rps.py
@@ -5,6 +5,10 @@ import numpy as np
 import random
 
 class RPS:
+  """
+  This class holds all the functions and variables required for the game. To change the number of rounds in a match, refer
+  to the 'run_game' function below.
+  """
   def __init__(self) -> None:
     self.timeCheck = []
     self.playerWins = 0
@@ -12,12 +16,15 @@ class RPS:
 
   def get_computer_choice():
     '''
-    This function gets the computer's choice for the game and returns it as a string
+    This gets the computer's choice for the game and returns it as a string
     '''
     actions = ['rock', 'paper', 'scissors']
     return random.choice(actions)
   
   def countdown(self, start):
+    """
+    This creates the countdown that is printed in the terminal before the image is captured for the user's choice of rock, paper or scissors.
+    """
     duration = 3.5
     end_time = start + duration
     if int(end_time - time.time()) not in self.timeCheck:
@@ -25,7 +32,9 @@ class RPS:
       print(f"Capturing image in {int(end_time - time.time())} seconds")
   
   def display_webcam_and_capture_image(self, cap):
-    """Displays the webcam feed for 3 seconds and captures the final image."""
+    """
+    Displays the webcam feed for 3 seconds and captures the final image.
+    """
     # Capture frames for 3 seconds
     start_time = time.time()
     while (time.time() - start_time) < 3.5:
@@ -45,7 +54,12 @@ class RPS:
 
     return frame
   
-  def main(self):
+  def main_loop(self):
+    """
+    This is bulk of the game's code. We call the function to capture the player's image and then feed it to the keras model and convert the
+    result to a string result of the user's choice for the round. Note the self.timecheck is set to a blank list at the end of this function
+    in order reset the countdown function.
+    """
     input("Press enter when you're ready for the round!")
     cap = cv2.VideoCapture(0)  # Open webcam
 
@@ -77,6 +91,9 @@ class RPS:
     self.timeCheck = []
     
   def get_winner(self, user , computer):
+    """
+    Gets the winner of the round and adds 1 to the winner's variable counter.
+    """
     if computer == 'rock' and user == 'paper':
         self.playerWins += 1
         print(f'User wins! Player threw {user}, Computer threw {computer}')
@@ -104,8 +121,9 @@ class RPS:
         
   def run_game(self):
     """
-    This is the main function you run to play the game, it contains the main loop for the game. Ends when someone wins 3 times.
-    If you want to change number of wins, change the number at the top of the while loop. 
+    This is the main function you run to play the game. Ends when someone wins 3 times.
+    If you want to change number of wins, change the number below in the while loop 
+    from 3 (for both) to any number you wish. 
     """
     print(
           """
@@ -117,7 +135,7 @@ class RPS:
           )
     
     while self.playerWins < 3 and self.computerWins < 3:
-      self.main()
+      self.main_loop()
     if self.playerWins == 3:
       print("You win!")
     else:
